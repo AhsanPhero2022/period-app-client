0e173803678cb3d559e3f7bdb07f137dbc194222
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-01-14 16:28:29 +0600

    consul request table done

--- src/components/UserDashboard/ConsulRequest/SeeConsult.jsx
+++ src/components/UserDashboard/ConsulRequest/SeeConsult.jsx
@@ -1,197 +1,67 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 const SeeConsult = () => {
   const [data, setData] = useState([]);
+  
+  
 
   useEffect(() => {
     fetch("http://localhost:5000/consultantRequest")
       .then((res) => res.json())
-      .then((data) => setData(data));
+      .then((newData) => setData(newData));
   }, []);
-
-   
 
   return (
     <div className="overflow-x-auto">
-      <table className="table">
+          <table className="table">
         {/* head */}
         <thead>
           <tr>
-           
-            <th>Name</th>
-            <th>Job</th>
+            <th>Image</th>
+            <th>Description</th>
             <th>date</th>
             <th>Accept</th>
-            <th>Okay</th>
+            <th>Accept</th>
           </tr>
         </thead>
         <tbody>
           {/* row 1 */}
-          <tr>
-          
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img
-                      src=""
-                      alt="Avatar Tailwind CSS Component"
-                    />
+          {data.map((item, index) => (
+            <tr key={index}>
+                <td>
+                <div className="flex items-center gap-3">
+                  <div className="avatar">
+                    <div className="mask mask-squircle w-12 h-12">
+                      <img src="" alt="Avatar Tailwind CSS Component" />
+                    </div>
+                  </div>
+                  <div>
+                    <div className="font-bold">
+                      <h1>{item.patientName}</h1>
+                    </div>
+                    <div className="text-sm opacity-50">United States</div>
                   </div>
                 </div>
-                <div>
-                  <div className="font-bold">he</div>
-                  <div className="text-sm opacity-50">United States</div>
-                </div>
-              </div>
-            </td>
-            <td>
-              Zemlak, Daniel and Leannon
-              <br />
-              <span className="badge badge-ghost badge-sm">
-                Desktop Support Technician
-              </span>
-            </td>
-            <th>
-              <button className="btn btn-warning btn-xs">accept</button>
-            </th>
-            <th>
-              <button className="btn btn-success btn-xs">Deny</button>
-            </th>
-          </tr>
-          {/* row 2 */}
-          <tr>
-          
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img
-                      src="/tailwind-css-component-profile-3@56w.png"
-                      alt="Avatar Tailwind CSS Component"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold">Brice Swyre</div>
-                  <div className="text-sm opacity-50">China</div>
-                </div>
-              </div>
-            </td>
-            <td>
-              Carroll Group
-              <br />
-              <span className="badge badge-ghost badge-sm">Tax Accountant</span>
-            </td>
-            <td>Red</td>
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
-            </th>
-          </tr>
-          {/* row 3 */}
-          <tr>
-          
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img
-                      src="/tailwind-css-component-profile-4@56w.png"
-                      alt="Avatar Tailwind CSS Component"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold">Marjy Ferencz</div>
-                  <div className="text-sm opacity-50">Russia</div>
-                </div>
-              </div>
-            </td>
-            <td>
-              Rowe-Schoen
-              <br />
-              <span className="badge badge-ghost badge-sm">
-                Office Assistant I
-              </span>
-            </td>
-            <td>Crimson</td>
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
-            </th>
-          </tr>
-          {/* row 4 */}
-          <tr>
-          
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img
-                      src="/tailwind-css-component-profile-4@56w.png"
-                      alt="Avatar Tailwind CSS Component"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold">Marjy Ferencz</div>
-                  <div className="text-sm opacity-50">Russia</div>
-                </div>
-              </div>
-            </td>
-            <td>
-              Rowe-Schoen
-              <br />
-              <span className="badge badge-ghost badge-sm">
-                Office Assistant I
-              </span>
-            </td>
-            <td>Crimson</td>
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
-            </th>
-          </tr>
-          {/* row 5 */}
-          <tr>
-          
-            <td>
-              <div className="flex items-center gap-3">
-                <div className="avatar">
-                  <div className="mask mask-squircle w-12 h-12">
-                    <img
-                      src="/tailwind-css-component-profile-5@56w.png"
-                      alt="Avatar Tailwind CSS Component"
-                    />
-                  </div>
-                </div>
-                <div>
-                  <div className="font-bold">Yancy Tear</div>
-                  <div className="text-sm opacity-50">Brazil</div>
-                </div>
-              </div>
-            </td>
-            <td>
-              Wyman-Ledner
-              <br />
-              <span className="badge badge-ghost badge-sm">
-                Community Outreach Specialist
-              </span>
-            </td>
-            <td>Indigo</td>
-            <th>
-              <button className="btn btn-ghost btn-xs">details</button>
-            </th>
-          </tr>
+              </td>
+              <td>
+                Zemlak, Daniel and Leannon
+                <br />
+                <span className="badge badge-ghost badge-sm">
+                  Desktop Support Technician
+                </span>
+              </td>
+              <th>
+                <button className="">Date: {item.selectedDate}</button>
+              </th>
+              <th>
+                <button className="btn btn-success btn-xs">Accept</button>
+              </th>
+              <th>
+                <button className="btn btn-success btn-xs">Deny</button>
+              </th>
+            </tr>
+          ))}
         </tbody>
-        {/* foot */}
-        <tfoot>
-          <tr>
-            <th></th>
-            <th>Name</th>
-            <th>Job</th>
-            <th>Favorite Color</th>
-            <th></th>
-          </tr>
-        </tfoot>
       </table>
     </div>
   );
