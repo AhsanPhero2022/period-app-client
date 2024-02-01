f17cb0110d1ed5c86ce3201d03d4c3fd835d54f8
Sharif Ahmed <97349171+AhsanPhero2022@users.noreply.github.com>
2024-02-01 21:49:48 +0600

    profile updating

--- src/components/UserDashboard/UserProfile.jsx
+++ src/components/UserDashboard/UserProfile.jsx
@@ -1,7 +1,6 @@
-/* eslint-disable no-unused-vars */
-import React, { useContext } from "react";
+
 import Avatar from "@material-ui/core/Avatar";
-import Calendar from "../../calendar/Calendar";
+import { NoteModul } from "./UserProfileFeauturs/NoteModul";
 
 
 function UserProfile() {
@@ -9,7 +8,7 @@ function UserProfile() {
  
   return (
     <div className="grid my-8 gap-4 lg:grid-cols-3">
-      <div className=" w-[330px]  bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 rounded-lg ms-2">
+      <div className="  bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 rounded-lg ms-2">
         <div className=" ">
           <Avatar
             className="mx-auto mt-4"
@@ -45,7 +44,7 @@ function UserProfile() {
           </button>
         </div>
       </div>
-      <div className="ms-24 ps-5 bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 rounded-lg">
+      <div className=" ps-5 bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 rounded-lg">
         <div className="mt-3">
           <h2>Home Address</h2>
           <p>123 Broadway </p>
@@ -69,52 +68,31 @@ function UserProfile() {
       </div>
       <div className="flex flex-col justify-center items-center gap-3">
         <div className="">
-          <div className="w-60 bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 grid grid-col-2 justify-center p-4 gap-4 rounded-lg shadow-md">
+          <div className=" bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 grid grid-col-2 justify-center p-4 gap-4 rounded-lg shadow-md">
             <div className="col-span-2 text-lg font-bold capitalize rounded-md">
               Hello world
             </div>
-            <div className="col-span-2 rounded-md">
+            <div className=" rounded-md">
               Using Lorem ipsum to focus attention on graphic elements in a
               webpage design
             </div>
           </div>
         </div>
         <div className="">
-          <div className=" ">
-            <div className="w-60 bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 grid grid-col-2 justify-center p-4 gap-4 rounded-lg shadow-md">
-              <div className="col-span-2 text-lg font-bold capitalize rounded-md">
+          
+            <div className=" bg-gradient-to-l from-slate-300 to-slate-100 text-slate-600 border border-slate-300 grid grid-col-2 justify-center p-4 gap-4 rounded-lg shadow-md">
+              <div className="hover:bg-green-500  text-lg font-bold capitalize rounded-md">
                 Hello world
               </div>
-              <div className="col-span-2 rounded-md">
+              <div className=" rounded-md">
                 Using Lorem ipsum to focus attention on graphic elements in a
                 webpage design
               </div>
             </div>
-          </div>
+          
         </div>
       </div>
-      {/* text area */}
-      
-     {/* Open the modal using document.getElementById('ID').showModal() method */}
-<button className="btn btn-success" onClick={()=>document.getElementById('my_modal_5').showModal()}>Add Note</button>
-<dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
-  <div className="modal-box">
-    <h3 className="font-bold text-lg">Hello!</h3>
-    <p className="py-4">Press ESC key or click the button below to close</p>
-    <div className="my-2">
-    <input type="text" placeholder="Title" className="input input-bordered input-success  max-w-xs" />
-     <Calendar></Calendar>
-    </div>
-    <textarea className="textarea textarea-success w-full" placeholder="Add Your Daily Note"></textarea>
-    <div className="modal-action">
-      <form method="dialog">
-      
-        <button className="btn">Close</button>
-      </form>
-    </div>
-  </div>
-</dialog>
-      {/* Text are end*/}
+      <NoteModul/>
     </div>
   );
 }
---
+++ src/components/UserDashboard/UserProfileFeauturs/NoteModul.jsx
@@ -0,0 +1,39 @@
+import Calendar from "./Calendar";
+
+export const NoteModul = () => {
+  return (
+    <div>
+      <button
+        className="btn btn-success"
+        onClick={() => document.getElementById("my_modal_5").showModal()}
+      >
+        Add Note
+      </button>
+      <dialog id="my_modal_5" className="modal modal-bottom sm:modal-middle">
+        <div className="modal-box">
+          <h3 className="font-bold text-lg">Hello!</h3>
+          <p className="py-4">
+            Press ESC key or click the button below to close
+          </p>
+          <div className="my-2">
+            <input
+              type="text"
+              placeholder="Title"
+              className="input input-bordered input-success  max-w-xs"
+            />
+            <Calendar></Calendar>
+          </div>
+          <textarea
+            className="textarea textarea-success w-full"
+            placeholder="Add Your Daily Note"
+          ></textarea>
+          <div className="modal-action">
+            <form method="dialog">
+              <button className="btn">Close</button>
+            </form>
+          </div>
+        </div>
+      </dialog>
+    </div>
+  );
+};
